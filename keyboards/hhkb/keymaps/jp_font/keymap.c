3444b6601c049bddee4fd99c57cbfb0f99924939
Nic Fontaine <nic@nicfontaine.com>
2021-03-08 16:04:05 -0800

    top right keys, ignore binutils

--- keyboards/hhkb/keymaps/jp_font/keymap.c
+++ keyboards/hhkb/keymaps/jp_font/keymap.c
@@ -44,14 +44,14 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_JP(
-        KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_GRV, KC_DEL,
+        KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_MINS, KC_EQL,
         KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_G, KC_BSLS, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_MINS,
         KC_BSPC, KC_A, KC_R, KC_S, KC_T, KC_D, KC_PGUP, KC_H, KC_N, KC_E, KC_I, KC_O, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_PGDN, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP, KC_PSCR,
         KC_ESC, MO(2), KC_LGUI, KC_LALT, KC_LCTL, KC_SPC, MO(1), KC_RALT, KC_NO, TO(2), KC_LEFT, KC_DOWN, KC_RGHT),
 
     [1] = LAYOUT_JP(
-        _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS, KC_DEL,
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, _______, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL,
         _______, KC_VOLU, KC_MUTE, KC_MSTP, KC_MPLY, KC_PGUP, _______, KC_LBRC, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_GRV, KC_PENT,
         _______, KC_VOLD, KC_PAUSE, KC_MPRV, KC_MNXT, KC_PGDN, _______, KC_RBRC, KC_HOME, KC_END, _______, KC_BSLS, _______, _______,
